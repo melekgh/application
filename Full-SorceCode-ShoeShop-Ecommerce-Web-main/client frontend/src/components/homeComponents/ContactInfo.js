bbb0064b3d01337d6430aac78258457811482203
melekgh <melekghrab50@gmail.com>
2022-05-23 11:07:36 +0100

    first commit

---
+++ Full-SorceCode-ShoeShop-Ecommerce-Web-main/client frontend/src/components/homeComponents/ContactInfo.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const ContactInfo = () => {
+  return (
+    <div className="contactInfo container">
+      <div className="row">
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-phone-alt"></i>
+            </div>
+            <h5>Call Us 24x7</h5>
+            <p>+216 28384422</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-map-marker-alt"></i>
+            </div>
+            <h5>Adress</h5>
+            <p>Beja </p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-fax"></i>
+            </div>
+            <h5>Fax</h5>
+            <p>+216 28384422</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactInfo;
---
+++ Full-SorceCode-ShoeShop-Ecommerce-Web-main/dashboard/src/components/Categories/CategoriesTable.js
@@ -0,0 +1,124 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const CategoriesTable = () => {
+  return (
+    <div className="col-md-12 col-lg-8">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>
+              <div className="form-check">
+                <input className="form-check-input" type="checkbox" value="" />
+              </div>
+            </th>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th className="text-end">Action</th>
+          </tr>
+        </thead>
+        {/* Table Data */}
+        <tbody>
+          <tr>
+            <td>
+              <div className="form-check">
+                <input className="form-check-input" type="checkbox" value="" />
+              </div>
+            </td>
+            <td>1</td>
+            <td>
+              <b>Matériels</b>
+            </td>
+            <td>Matériels</td>
+            <td className="text-end">
+              <div className="dropdown">
+                <Link
+                  to="#"
+                  data-bs-toggle="dropdown"
+                  className="btn btn-light"
+                >
+                  <i className="fas fa-ellipsis-h"></i>
+                </Link>
+                <div className="dropdown-menu">
+                  <Link className="dropdown-item" to="#">
+                    Edit info
+                  </Link>
+                  <Link className="dropdown-item text-danger" to="#">
+                    Delete
+                  </Link>
+                </div>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className="form-check">
+                <input className="form-check-input" type="checkbox" value="" />
+              </div>
+            </td>
+            <td>2</td>
+            <td>
+              <b>Produits</b>
+            </td>
+            <td>Produits</td>
+
+            <td className="text-end">
+              <div className="dropdown">
+                <Link
+                  to="#"
+                  data-bs-toggle="dropdown"
+                  className="btn btn-light"
+                >
+                  <i className="fas fa-ellipsis-h"></i>
+                </Link>
+                <div className="dropdown-menu">
+                  <Link className="dropdown-item" to="#">
+                    Edit info
+                  </Link>
+                  <Link className="dropdown-item text-danger" to="#">
+                    Delete
+                  </Link>
+                </div>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className="form-check">
+                <input className="form-check-input" type="checkbox" value="" />
+              </div>
+            </td>
+            <td>3</td>
+            <td>
+              <b></b>
+            </td>
+            <td></td>
+
+            <td className="text-end">
+              <div className="dropdown">
+                <Link
+                  to="#"
+                  data-bs-toggle="dropdown"
+                  className="btn btn-light"
+                >
+                  <i className="fas fa-ellipsis-h"></i>
+                </Link>
+                <div className="dropdown-menu">
+                  <Link className="dropdown-item" to="#">
+                    Edit info
+                  </Link>
+                  <Link className="dropdown-item text-danger" to="#">
+                    Delete
+                  </Link>
+                </div>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default CategoriesTable;
